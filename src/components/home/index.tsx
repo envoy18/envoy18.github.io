f3a74c6f5d1ced375308d5f4984fa194e656e641
caretsolutions <jason.ampalayohan@gmail.com>
2025-04-21 14:42:05 +0900

    fixed config

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-import { useInView } from 'react-intersection-observer';
+//import { useInView } from 'react-intersection-observer';
 
 export const Home: React.FC = () => {
-    const { ref, inView } = useInView({
-        threshold: 0,
-    });
+    // const { ref, inView } = useInView({
+    //     threshold: 0,
+    // });
 
     return (
-        <div ref={ref} className={`w-full sm:w-1/2 pt-20 px-10 sm:px-0`}>
+        <div className={`w-full sm:w-1/2 pt-20 px-10 sm:px-0`}>
             <div className="flex flex-col text-white font-mono text-left">
                 <div>
                     <p className="text-3xl font-black"> 
